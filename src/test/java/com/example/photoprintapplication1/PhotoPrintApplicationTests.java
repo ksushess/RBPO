799f4f2cec7c7f15d8c3603f29cb294c57596adc
ksushess <bochkareva-k@bk.ru>
2025-12-18 21:38:35 +0300

    ci2

--- src/test/java/com/example/photoprintapplication1/PhotoPrintApplicationTests.java
+++ src/test/java/com/example/photoprintapplication1/PhotoPrintApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.PhotoPrintApplication;
+package com.example.photoprintapplication1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
