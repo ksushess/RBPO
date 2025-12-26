d709374dfd7e6419189d19f1812af18b846448d9
ksushess <bochkareva-k@bk.ru>
2025-12-26 17:56:37 +0300

    fixCI

--- src/test/java/com/example/photoprintapplication1/PhotoPrintApplicationTests.java
+++ src/test/java/com/example/photoprintapplication1/PhotoPrintApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.photoprintapplication1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest (properties = "server.ssl.enabled=false")
+@SpringBootTest (classes = PhotoprintApplication.class)
 class PhotoPrintApplicationTests {
 
 	@Test
